494f3a002782e03eb2714620de6a0fb271fe46ec
PHANZzz <sunhout3344@gmail.com>
2024-05-10 00:15:34 +0700

    fixed error page

--- src/app/search/error.jsx
+++ src/app/search/error.jsx
@@ -1,16 +1,16 @@
-'use client'
-import { useEffect } from 'react'
-import React from 'react'
+'use strict';
+import { useEffect } from 'react';
+import React from 'react';
 
-export default function error({error,reset}) {
-    useEffect(() =>{
-   console.log('error',error);
-    },[error]);
-    
-  return (
-    <div className='flex flex-col items-center justify-center pt-10'>
-        <h1 className='mb-4 text-3xl'>Something went wrong</h1>
-        <button className='text-blue-500'>Try again</button>
-    </div>
-  )
+export default function Error({error, reset}) {
+    useEffect(() => {
+        console.log('error', error);
+    }, [error]);
+
+    return (
+        <div className='flex flex-col items-center justify-center pt-10'>
+            <h1 className='mb-4 text-3xl'>Something went wrong</h1>
+            <button className='text-blue-500' onClick={reset}>Try again</button>
+        </div>
+    );
 }
