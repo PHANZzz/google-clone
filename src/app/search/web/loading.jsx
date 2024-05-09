c080a39b6cce09668c2f2dc1711bb435089813a3
PHANZzz <sunhout3344@gmail.com>
2024-05-10 01:01:09 +0700

    complete loading effect

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className='flex items-center justify-center h-[80vh]'>
+      <div className='text-3xl text-blue-500 font-lg'>Loading...</div>
+    </div>
+  );
+}
